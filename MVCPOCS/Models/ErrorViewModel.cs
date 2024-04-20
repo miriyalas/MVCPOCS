db418edc26ff83dad8cc4deb465da823bd428ff3
vanis <vanis@SANDEEPMIRIYALA>
2024-04-20 17:09:59 +0530

    Add project files.

---
+++ MVCPOCS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCPOCS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
